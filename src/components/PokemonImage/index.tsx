9f22470e86180e9407d664b03a365a2364ab0302
Neil Swart <neilswart3@gmail.com>
2024-09-18 22:47:02 +0200

    fix color

--- src/components/PokemonImage/index.tsx
+++ src/components/PokemonImage/index.tsx
@@ -6,80 +6,76 @@ interface Props extends HTMLAttributes<HTMLImageElement> {
   src: string;
   alt: string;
   backdrop?: string[];
-  backDropColor?: string[];
 }
 
 export const PokemonImage: React.FC<Props> = ({
   src,
   alt,
   backdrop = [],
-  color = [],
   ...props
-}) => {
-  return (
-    <span
-      className={classNames(
-        'relative',
-        'flex',
-        'items-center',
-        'justify-center',
-        'z-0',
-      )}
-    >
-      {!!backdrop?.length && (
-        <span className="absolute opacity-25 translate-x-2 translate-y-4">
-          <span
-            className={classNames(
-              'flex',
-              'items-center',
-              'justify-center',
-              'absolute',
+}) => (
+  <span
+    className={classNames(
+      'relative',
+      'flex',
+      'items-center',
+      'justify-center',
+      'z-0',
+    )}
+  >
+    {!!backdrop?.length && (
+      <span className="absolute opacity-25 translate-x-2 translate-y-4">
+        <span
+          className={classNames(
+            'flex',
+            'items-center',
+            'justify-center',
+            'absolute',
 
-              'before:absolute',
-              'before:bg-white',
-              'before:w-52',
-              'before:h-52',
-              'before:rounded-full',
-              'before:z-[-1]',
+            'before:absolute',
+            'before:bg-white',
+            'before:w-52',
+            'before:h-52',
+            'before:rounded-full',
+            'before:z-[-1]',
 
-              'after:absolute',
-              'after:w-52',
-              'after:h-5',
-              color?.at(1),
-            )}
-          />
-          <span
-            className={classNames(
-              'flex',
-              'items-center',
-              'justify-center',
-              'absolute',
+            'after:absolute',
+            'after:w-52',
+            'after:h-5',
+            backdrop?.at(1),
+          )}
+        />
+        <span
+          className={classNames(
+            'flex',
+            'items-center',
+            'justify-center',
+            'absolute',
 
-              'before:absolute',
-              'before:rounded-full',
-              'before:w-24',
-              'before:h-24',
-              color?.at(0),
+            'before:absolute',
+            'before:rounded-full',
+            'before:w-24',
+            'before:h-24',
+            backdrop?.at(0),
 
-              'after:absolute',
-              'after:bg-white',
-              'after:rounded-full',
-              'after:w-14',
-              'after:h-14',
-            )}
-          />
-        </span>
-      )}
-      <span className="flex max-h-32 max-w-32">
-        <Image
-          src={src}
-          alt={alt}
-          height={150}
-          width={150}
-          className="relative z-1"
-          {...props}
+            'after:absolute',
+            'after:bg-white',
+            'after:rounded-full',
+            'after:w-14',
+            'after:h-14',
+          )}
         />
       </span>
+    )}
+    <span className="flex max-h-32 max-w-32">
+      <Image
+        src={src}
+        alt={alt}
+        height={150}
+        width={150}
+        className="relative z-1"
+        {...props}
+      />
     </span>
-  );
-};
+  </span>
+);
