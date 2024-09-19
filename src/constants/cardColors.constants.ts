6bbb4c525b1b9aa0500fcfb68c916902145da4ce
Neil Swart <neilswart3@gmail.com>
2024-09-19 19:30:05 +0200

    add constants

---
+++ src/constants/cardColors.constants.ts
@@ -0,0 +1,82 @@
+export const CARD_COLORS = {
+  grass: {
+    main: 'bg-emerald-400',
+    before: 'before:bg-emerald-400',
+    after: 'after:bg-emerald-400',
+  },
+  fire: {
+    main: 'bg-red-400',
+    before: 'before:bg-red-400',
+    after: 'after:bg-red-400',
+  },
+  water: {
+    main: 'bg-sky-400',
+    before: 'before:bg-sky-400',
+    after: 'after:bg-sky-400',
+  },
+  bug: {
+    main: 'bg-lime-400',
+    before: 'before:bg-lime-400',
+    after: 'after:bg-lime-400',
+  },
+  normal: {
+    main: 'bg-gray-400',
+    before: 'before:bg-gray-400',
+    after: 'after:bg-gray-400',
+  },
+  poison: {
+    main: 'bg-purple-600',
+    before: 'before:bg-purple-600',
+    after: 'after:bg-purple-600',
+  },
+  ghost: {
+    main: 'bg-purple-600',
+    before: 'before:bg-purple-600',
+    after: 'after:bg-purple-600',
+  },
+  electric: {
+    main: 'bg-yellow-400',
+    before: 'before:bg-yellow-400',
+    after: 'after:bg-yellow-400',
+  },
+  ground: {
+    main: 'bg-orange-700',
+    before: 'before:bg-orange-700',
+    after: 'after:bg-orange-700',
+  },
+  rock: {
+    main: 'bg-orange-700',
+    before: 'before:bg-orange-700',
+    after: 'after:bg-orange-700',
+  },
+  fairy: {
+    main: 'bg-pink-400',
+    before: 'before:bg-pink-400',
+    after: 'after:bg-pink-400',
+  },
+  fighting: {
+    main: 'bg-indigo-600',
+    before: 'before:bg-indigo-600',
+    after: 'after:bg-indigo-600',
+  },
+  psychic: {
+    main: 'bg-pink-600',
+    before: 'before:bg-pink-600',
+    after: 'after:bg-pink-600',
+  },
+  ice: {
+    main: 'bg-cyan-300',
+    before: 'before:bg-cyan-300',
+    after: 'after:bg-cyan-300',
+  },
+  dragon: {
+    main: 'bg-teal-600',
+    before: 'before:bg-teal-600',
+    after: 'after:bg-teal-600',
+  },
+  default: {
+    main: 'bg-gray-300',
+    before: 'before:bg-gray-300',
+    after: 'after:bg-gray-300',
+  },
+};
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,2 @@
 export * from './styling.constants';
+export * from './cardColors.constants';
