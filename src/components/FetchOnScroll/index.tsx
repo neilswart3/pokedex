7aca9dae98e98924a120bf83579e7cd571711fb4
Neil Swart <neilswart3@gmail.com>
2024-09-19 20:21:33 +0200

    cleanup

--- src/components/FetchOnScroll/index.tsx
+++ src/components/FetchOnScroll/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { usePokemon, usePokemonDispatch } from '@/context';
-import { PokemonActionType } from '@/context/PokemonContext/types';
+import { usePokemon } from '@/context';
 import { useCallback, useEffect, useRef } from 'react';
 
 interface Props {
@@ -14,20 +13,6 @@ export const FetchOnScroll: React.FC<Props> = ({ name, url }) => {
   const init = useRef<boolean>(false);
 
   const [, { fetchPokemonItem }] = usePokemon();
-
-  //   const handleFetchData = useCallback(async () => {
-  //     try {
-  //       const data = await fetch(url);
-  //       const pokemonData = await data.json();
-
-  //       dispatch({
-  //         type: PokemonActionType.UPDATE_POKEMON,
-  //         payload: { data: pokemonData },
-  //       });
-  //     } catch (error) {
-  //       console.log('error:', error);
-  //     }
-  //   }, [dispatch, url]);
 
   const handleScroll = useCallback(() => {
     const parentOffsetTop = ref.current?.parentElement?.offsetTop || 0;
